e25a7717ef61b28449f057a0da378ef43537a23b
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-08-01 16:50:51 -0700

    pig latin

--- src/app.js
+++ src/app.js
@@ -3,53 +3,57 @@ import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-
 class App extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        value: 'Please write an essay about your favorite DOM element.',
-        newValue:'some new value'
+        value: 'Translate to Pig Latin',
+        wordArray: []
       };
-      this.words ='';
+      this.words = '';
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
+
     handleChange(event) {
-       this.setState({value: event.target.value});
-       this.words = this.state.value;
-       console.log(typeof this.words);
-       console.log(this.words);
+      this.setState({
+        value: event.target.value
+      });
     }
-  
+
     handleSubmit(event) {
-      this.setState({newValue:this.renderValue});
-     event.preventDefault();
-  
-    callBackFunction = (match,group1,group2) => {
-      console.log(`group 1 ${group1}`);
-      console.log(`group 2 ${group2}`);
- };
- let regInput =/\w+/g
- var regex = this.words.replace(regInput, this.callBackFunction);
-};
-  
+      this.words = this.state.value;
+      let array = this.words.split(/\s+/g).map((word) => {
+        if (/^[aeiou]/i.test(word)) {
+          return word + 'way';
+        } else {
+          return word.replace(/^([^aeiou]+)(.*)/i, '$2$1ay');
+        }
+      });
+
+      this.setState({
+        wordArray: array
+      });
+      event.preventDefault();
+    }
     render() {
       return (
-        <div>
-        <h1>Word Thingy</h1>
+   <div className='container'>
+        <h1>Pig Latin Translator</h1>
         <form onSubmit={this.handleSubmit}>
           <label>
-            Insert Text Here 
+             Text to Translate
             <textarea value={this.state.value} onChange={this.handleChange} />
           </label>
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Translate" />
         </form>
-        <p className='word-wrap'>{this.state.newValue}</p>
-        </div>
+        <p className='overflow-wrap:break-word;'> {this.state.wordArray.map((word,index)=>{
+              return <span key={index}>{word}{'\u00a0'}</span> 
+            })}</p>
+          </div>
       );
     }
   }
   ReactDOM.render(<App/>, document.getElementById('app'));
+  
   
